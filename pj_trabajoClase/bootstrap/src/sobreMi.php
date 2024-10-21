07cfe7d32cee7e9fb65b0c07da3367cfe0270d13
2DAM-andresegi295 <aegifer0806@g.educaand.es>
2024-10-21 21:57:50 +0200

    21/10

--- pj_trabajoClase/bootstrap/src/sobreMi.php
+++ pj_trabajoClase/bootstrap/src/sobreMi.php
@@ -1,7 +1,59 @@
 <?php
     $clase="sobreMi";
     require 'includes/header.php';
+
+    $agenda=array(
+        array(
+        "nombre"=>"Fútbol",
+        "descripcion"=> "Jugar al futbol con amigos",
+        "imagen"=> "./img/futbol.jpg",
+        "veces_semana"=>"3"
+        ),
+        array(
+            "nombre"=>"Baloncesto",
+            "descripcion"=> "Meter pelota en la canasta",
+            "imagen"=> "./img/baloncesto.jpeg",
+            "veces_semana"=>"2"
+        ),
+        array(
+            "nombre"=>"Ciclismo",
+            "descripcion"=> "Montar en bicicleta por el campo",
+            "imagen"=> "./img/ciclismo.jpg",
+            "veces_semana"=>"4"
+        )
+    );
+
+    function array_orderby($arrayToOrder, $field) {
+        $columna=array_column($arrayToOrder, $field);
+
+        array_multisort($columna, SORT_ASC,  $arrayToOrder);
+        return $arrayToOrder;
+    }
+
+    $agendas_or=array_orderby($agenda,"veces_semana")
+
 ?>
-<h2>SOBRE MI</h2>
-</body>
-</html>
+<div class="container">
+    <h1 class="text-center mb-4"></h1>
+    <div class="row">
+        <?php
+            foreach($agendas_or as $clave) {
+                //echo $clave["nombre"]."<br>";
+                $nombre=$clave["nombre"];
+                $descripcion=$clave["descripcion"];
+                $veces=$clave['veces_semana'];
+                $imagen=$clave["imagen"];
+                echo "<div class='col-12 col-md-4'>
+                <div class='card' style='width: 18rem;'>
+                <img src='$imagen' class='card-img-top' alt='...'>
+                <div class='card-body'>
+                  <h5 class='card-title'>$nombre</h5>
+                  <p class='card-text'>$descripcion</p>
+                  <a href='#' class='btn btn-primary'>$veces</a>
+                </div>
+              </div>
+              </div>";
+            }
+        ?>
+    </div>
+</div>
