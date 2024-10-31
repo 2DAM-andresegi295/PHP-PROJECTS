a8ecf0a533db50109a9a73c8fe9c6575063d1a9e
2DAM-andresegi295 <aegifer0806@g.educaand.es>
2024-10-31 21:56:13 +0100

    31/10

--- pj_trabajoClase/bootstrap/src/index.php
+++ pj_trabajoClase/bootstrap/src/index.php
@@ -1,42 +1,60 @@
 <?php
-    $clase="inicio";
-    require 'includes/header.php';
+$clase = "inicio";
+require 'includes/header.php';
 
-    $persona=array(
-        "nombre"=>"maria",
-        "apellido"=> "millan",
-        "correo"=> "aaaa@gmail.com"
-    );
-    foreach($persona as $clave => $p) {
-        echo $clave."-".$p."<br>";
-    }
+$persona = array(
+    "nombre" => "maria",
+    "apellido" => "millan",
+    "correo" => "aaaa@gmail.com"
+);
+foreach ($persona as $clave => $p) {
+    echo $clave . "-" . $p . "<br>";
+}
 
-    $agenda=array(
-        array(
-        "nombre"=>"maria",
-        "apellido"=> "millan",
-        "correo"=> "aaaa@gmail.com"
-        ),
-        array(
-            "nombre"=>"maria",
-            "apellido"=> "millan",
-            "correo"=> "aaaa@gmail.com"
-        ),
-        array(
-            "nombre"=>"maria",
-            "apellido"=> "millan",
-            "correo"=> "aaaa@gmail.com"
-        )
-    );
+$agenda = array(
+    array(
+        "nombre" => "maria",
+        "apellido" => "millan",
+        "correo" => "aaaa@gmail.com"
+    ),
+    array(
+        "nombre" => "maria",
+        "apellido" => "millan",
+        "correo" => "aaaa@gmail.com"
+    ),
+    array(
+        "nombre" => "maria",
+        "apellido" => "millan",
+        "correo" => "aaaa@gmail.com"
+    )
+);
 
-    foreach($agenda as $clave => $valor) {
+foreach ($agenda as $clave => $valor) {
 
-        echo $valor['nombre'].$valor['apellido'].$valor['correo']."<br>";
+    echo $valor['nombre'] . $valor['apellido'] . $valor['correo'] . "<br>";
 
-    }
+}
 ?>
 
 
 <h2>INDEX</h2>
+<div class="container">
+    <div class="row">
+        <?php
+        $directorio = "./img/";
+        $dir = scandir("./img/");
+        foreach ($dir as $file) {
+            $extension = pathinfo($file, PATHINFO_EXTENSION);
+            if ($extension == "jpg" || $extension == "jpeg") {
+                $ruta = $directorio . $file;
+                echo "  <div class = col-3>
+                                    <img src=$ruta>
+                                </div>";
+            }
+        }
+        ?>
+    </div>
+</div>
 </body>
+
 </html>
